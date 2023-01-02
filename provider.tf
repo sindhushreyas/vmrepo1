87fa51a421404714835fd6e72a720410340e33b1
sindhushreyas <116134520+sindhushreyas@users.noreply.github.com>
2023-01-02 11:43:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      source = "hashicorp/azurerm"
+      version = "~> 3.37.0"
     }
   }
-
   required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  features {}
+   features {}
+  # Configuration options
 }
-
 
